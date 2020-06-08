b29f1751db9658527376e72366cd95c451b5d09c
Javier <50845491+jasvan@users.noreply.github.com>
2020-06-08 11:00:36 -0500

    proyecto inicial

---
+++ app/src/main/java/com/example/helloword/MainActivity.java
@@ -0,0 +1,77 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+package com.example.helloword;
+import java.text.NumberFormat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+/**
+ * This app displays an order form to order coffee.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+
+        int number = displayPrice(quantity);
+
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+        displayQuantity(quantity);
+    }
+
+    public void submitIncrement(View view) {
+            quantity +=  1;
+        displayQuantity(quantity);
+    }
+
+    public void submitDecrement(View view) {
+            quantity -=  1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+
+    private int displayPrice(int price) {
+        int total = price * 1;
+        return total;
+    }
+
+}
+
+
+
+
+
+
+
+
+
